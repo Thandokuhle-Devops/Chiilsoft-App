e2d8cdbf90a1bff765897d4fcc8caf4773026a14
Thandokuhle-Devops <149593958+Thandokuhle-Devops@users.noreply.github.com>
2023-11-01 16:57:31 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chillisoft.Startup))]
+namespace Chillisoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
